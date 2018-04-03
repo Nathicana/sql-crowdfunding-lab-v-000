f656f16768107b8eb76c17fde5c99c35071e337c
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-03 01:06:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, SUM(pledges.amount)
 FROM projects
 JOIN pledges ON projects.id = pledges.project_id
- WHERE category = 'music';
+ GROUP BY category WHERE category = 'music';
 "
 end
 
